9c9f5f6dc8cb56d167e2634ac84d892762f39237
DiegoFalconleon <0333172009@unjfsc.edu.pe>
2023-10-12 18:59:05 -0500

    asd

--- app/Http/Controllers/company/CompanyController.php
+++ app/Http/Controllers/company/CompanyController.php
@@ -9,8 +9,7 @@ use App\Models\Company;
 class CompanyController extends Controller
 {
     public function index(){
-      $company = Company::all();
-      dd($company);
+      $company = Company::find(1);
       return view('content.company.index', compact('company'));
     }
 
